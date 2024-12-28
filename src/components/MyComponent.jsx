c538212d23f19dbb6adfd2f656618ff65cf99099
rajubansacharya <rajubansacharya99@gmail.com>
2024-12-28 08:17:25 +0545

    I have done simple to do app with adding and deleting the task

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,35 +1,55 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+
 
 function MyComponent() {
+  const [addList, setAddList] = useState([]);
+  const [enteredData, setEnteredData] = useState("");
 
-    const [foods, setFoods] = useState(["Apple" , "Banana" , "Cherry"]);
-
-    function handleAddFood(){
-        const givenFood = document.getElementById("foodInput").value;
-        document.getElementById("foodInput").value = "";
-
-        setFoods(f => [...f,givenFood]);
+  function addDataInList() {
+    if (enteredData.trim()) {
+      setAddList([...addList, enteredData]);
+      setEnteredData("");
     }
-    function handleRemoveFood(index){
-            setFoods(foods.filter((_, i) => i !==index));
-    }
-    
-    return(
-        <div>
-            <h1>List of the food are Give below: </h1>
-            <ul>
-                {foods.map ((food , index) => <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>)}
-            </ul>
-            <input type="text" id="foodInput" placeholder="Enter food name" />
-            <button onClick={handleAddFood}> Add food </button>
+  }
 
+  function enterdDataField(event) {
+    setEnteredData(event.target.value);
+  }
 
-            
-        </div>
-        
-    );
+  function deleteEnteredData(index) {
+    setAddList(addList.filter((_, i) => i !== index));
+  }
 
-
+  return (
+    <div className="todo-container">
+      <h1 className="todo-header">TO-DO LIST</h1>
+      <div className="todo-input-container">
+        <input
+          className="todo-input"
+          type="text"
+          placeholder="Enter your task here"
+          value={enteredData}
+          onChange={enterdDataField}
+        />
+        <button className="todo-add-button" onClick={addDataInList}>
+          Add
+        </button>
+      </div>
+      <div className="todo-list-container">
+        {addList.map((item, index) => (
+          <div className="todo-item" key={index}>
+            <span className="todo-task">{item}</span>
+            <button
+              className="todo-delete-button"
+              onClick={() => deleteEnteredData(index)}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default MyComponent
+export default MyComponent;
